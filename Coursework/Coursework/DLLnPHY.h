4d21bef700a69543fcd69c5d4754d3c583c6a3cb
travis3630 <43075484+travis3630@users.noreply.github.com>
2022-06-06 08:55:15 +0100

    Update DLLnPHY.h

--- Coursework/Coursework/DLLnPHY.h
+++ Coursework/Coursework/DLLnPHY.h
@@ -15,7 +15,7 @@
 uint8_t node_addr = NODE1;
 uint8_t HeaderFooter = 0x7E;
 uint8_t flagbyte = 0x55;
-uint8_t NET_packet[] = {
+uint8_t* NET_packet[] = {
     0x74, 0x64,
     0x01, 0x0F,   // src and dest addr
     0x79,         // length
@@ -35,23 +35,23 @@ uint8_t NET_packet[] = {
 typedef struct Packet{
     uint8_t Header;  // 1 byte
     uint8_t Footer;  // 1 byte
-    uint8_t Netpkt[];       // 1-23bytes
-    uint8_t checksum[2];    // 2 bytes
+    uint8_t* Netpkt[];       // 1-23bytes
+    uint8_t* checksum[2];    // 2 bytes
     uint8_t Length;         // 1 byte
-    uint8_t Address[2];     // 2 bytes
-    uint8_t Control[2];     // 2 bytes
+    uint8_t* Address[2];     // 2 bytes
+    uint8_t* Control[2];     // 2 bytes
     uint8_t Everything[];   // bytes
 } Packet;
 
 class DLL {       // The class
     public:       // Access specifier
         DLL();    //constructor
-        void from_NET_layer(uint8_t networkpayload[]);
+        void from_NET_layer(uint8_t* networkpayload[]);
         Packet to_PHY_layer();
         uint8_t to_NET_layer();
     private:
         Packet Payload;
-        void from_NET_layer(uint8_t networkpayload[]);
+        void from_NET_layer(uint8_t* networkpayload[]);
         Packet to_PHY_layer();
 		uint8_t NET_queue_down[];
         uint8_t NET_queue_up[];
